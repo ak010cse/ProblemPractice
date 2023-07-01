78f84c2c099c3370ffd40f4b22cdbd322f6eeb5d
ak010cse <ak010cse@gmail.com>
2023-07-02 01:40:55 +0530

    Initial commit

--- app/src/main/java/com/android/problempractice/MainActivity.kt
+++ app/src/main/java/com/android/problempractice/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.problempractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.atoast.AToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AToast.showToast(this,"Hello Arvind, Welcome to AToast Library!!")
     }
 }
